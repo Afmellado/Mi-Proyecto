a980306cc53ae5eb15a13602d2fac61edbf43127
Your Actual Name <Your Actual Email>
2013-11-16 16:28:08 -0300

    adding slate boostrap, moving navbar to shared folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Proyecto::Application.routes.draw do
   get "pages/index"
   root 'pages#index'
+  get 'aboutus' => 'pages#aboutus'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
